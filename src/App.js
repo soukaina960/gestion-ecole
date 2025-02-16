839284d1d723043f7c1b6eb1d974adb99f2d7ac7
DESKTOP-SLL5TUE\toshiba <soukainaessabiri01>
2025-02-16 15:54:44 +0100

    Suppression de la clé AWS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./navbar.js";
 import LandingPage from "./navbar.js";
+import Fonctionnalité from "./fonctionnaliter.js";
 
 function App() {
   return (
     <div>
       <LandingPage />
+      <Fonctionnalité/>
       {/* Autres sections de votre site */}
     </div>
   );
