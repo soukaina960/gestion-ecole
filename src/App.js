bea1881e4e421301b58c10c60bb3d791eb20f521
DESKTOP-SLL5TUE\toshiba <soukainaessabiri01>
2025-02-16 15:16:40 +0100

    ajouter

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./navbar.js";
+import LandingPage from "./navbar.js";
+
+function App() {
+  return (
+    <div>
+      <LandingPage />
+      {/* Autres sections de votre site */}
+    </div>
+  );
+}
+
+export default App;
