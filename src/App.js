eddaeca193459e8e0631d6c2574653cacc0e606d
DESKTOP-SLL5TUE\toshiba <soukainaessabiri01>
2025-03-25 14:26:42 +0000

    ajouterfooter

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import Navbar from "./navbar.js";
 import LandingPage from "./navbar.js";
 import Fonctionnalité from "./fonctionnaliter.js";
+import Footer from "./Footer.js";
 
 function App() {
   return (
     <div>
       <LandingPage />
       <Fonctionnalité/>
-      {/* Autres sections de votre site */}
+    < Footer/>
     </div>
   );
 }
